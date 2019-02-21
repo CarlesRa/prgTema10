541d89080412ae40bfc59b23003039b727bf91a1
carlesra <jcramos8282@gmail.com>
2019-02-21 19:07:44 +0100

    exercici01 fet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import exercicistema10.Exercici01;
+
 public class Main {
     public static void main(String[] args) {
-
+        Exercici01 exe01 = new Exercici01();
     }
 }
--- src/main/java/exercicistema10/Exercici01.java
+++ src/main/java/exercicistema10/Exercici01.java
@@ -1,4 +1,36 @@
 package exercicistema10;
 
+import utils.Lib;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Exercici01 {
+    public Exercici01(){
+        int [] vector = new int[10];
+        for (int i=0; i<vector.length; i++){
+            vector[i] = Lib.random(0,50);
+        }
+        System.out.println(Arrays.toString(vector));
+        System.out.println(vList(vector).toString());
+    }
+    public ArrayList <Integer> vList(int [] vector){
+        int puntero = vector.length-1;
+        int punteroP = 0;
+        ArrayList <Integer> aList = new ArrayList<Integer>(vector.length);
+        for (int z=0; z<vector.length; z++){
+            aList.add(z,0);
+        }
+        for (int i=0; i<vector.length; i++){
+            if (vector[i]%2==0){
+                aList.set(punteroP,vector[i]);
+                punteroP++;
+            }
+            else{
+                aList.set(puntero,vector[i]);
+                puntero--;
+            }
+        }
+        return aList;
+    }
 }
